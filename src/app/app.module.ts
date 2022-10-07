329894fbbed527d092c7810c0df32819d903da59
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2022-10-07 23:33:48 +0300

    signs & lights works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
 
-import { AppComponent } from './app.component';
-import { NordLightComponent } from './nord-light/nord-light.component';
+import { AppComponent } from "./app.component";
+import { NordLightComponent } from "./nord-light/nord-light.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NordLightComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NordLightComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
